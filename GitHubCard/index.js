22437a4a2259621454fce6b46b9352bd14bf5bdf
FAZ KAY <fkydln@gmail.com>
2020-10-20 17:10:38 -0700

    MVP met, I

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,20 +1,17 @@
-import axios from 'axios';
+import axios from "axios";
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
-const axiosPromise = axios.get('https://api.github.com/users/fkydln')
 
-.then((r)=>{
-  console.log("success!",);
-  console.log(r.data.bio);
-  var getData = r.data;
-  return getData;
-})
+const axiosPromise = axios.get("https://api.github.com/users/fkydln")
 
-console.log(getData);
-
+  .then((r) => {
+    console.log("success!");
+    var getData = r.data;
+    document.querySelector(".cards").appendChild(cardCreator(getData));
+  });
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -22,12 +19,10 @@ console.log(getData);
 
     Skip to STEP 3.
 */
-
 /*
   STEP 4: Pass the data received from Github into your function,
     and append the returned markup to the DOM as a child of .cards
 */
-
 /*
   STEP 5: Now that you have your own card getting added to the DOM, either
     follow this link in your browser https://api.github.com/users/<Your github name>/followers,
@@ -39,7 +34,17 @@ console.log(getData);
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = ["austenallred","jduell12","somersgreg","josiahroa18","jiayi-ren","dscromer","zoelud","aperez9423","srkn14"];
+
+followersArray.forEach(user => {
+     var axyProm = axios.get(`https://api.github.com/users/${user}`)
+      .then((response) => {
+        console.log(`pulled all!`);
+        
+        document.querySelector(".cards").appendChild(cardCreator(response.data))
+      })
+});
+
 
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
@@ -60,21 +65,54 @@ const followersArray = [];
       </div>
     </div>
 */
-const structure = function() {
-  const divCreator = document.createElement('div');
-  const imgCreator = document.createElement('img');
-  const h3Creator = document.createElement('h3');
-  const pCreator = document.createElement('p');
-  const aCreator = document.createElement('a');
+const cardCreator = (param) => {
+  const divCreator = document.createElement("div");
+  const divCreator2 = document.createElement('div');
+  const imgCreator = document.createElement("img");
+  const h3Creator = document.createElement("h3");
+  const pCreator = document.createElement("p");
+  const pCreator2 = document.createElement('p');
+  const pCreator3 = document.createElement('p');
+  const pCreator4 = document.createElement('p');
+  const pCreator5 = document.createElement('p');
+  const pCreator6 = document.createElement('p');
+  const aCreator = document.createElement("a");
 
-  divCreator.classList.add('card')
-  imgCreator.setAttribute("src", "")
 
-  document.body.appendChild(divCreator)
-  document.getElementsByClassName('card')[0].appendChild(imgCreator);
-}
+  divCreator.classList.add("card");
+  divCreator2.classList.add('card-info');
+  h3Creator.classList.add('name');
+  pCreator.classList.add('username');
 
-structure();
+
+
+  //imgCreator.setAttribute("src", param.avatar_url); does the same job.
+  imgCreator.src = param.avatar_url;
+  aCreator.href = param.url;
+  h3Creator.textContent = param.name;
+  pCreator.textContent = param.login;
+  pCreator2.textContent = `Location: ${param.location}`;
+  pCreator3.textContent = `Profile: `;
+  aCreator.textContent = param.html_url;
+  pCreator4.textContent = `Followers: ${param.followers}`;
+  pCreator5.textContent = `Following: ${param.following}`;
+  pCreator6.textContent = `Bio: ${param.bio}`
+
+
+  divCreator.appendChild(imgCreator);
+  divCreator.appendChild(divCreator2);
+  divCreator.querySelector('.card-info').appendChild(h3Creator);
+  divCreator.querySelector('.card-info').appendChild(pCreator);
+  divCreator.querySelector('.card-info').appendChild(pCreator2);
+  divCreator.querySelector('.card-info').appendChild(pCreator3);
+  divCreator.querySelectorAll('p')[2].appendChild(aCreator);
+  divCreator.querySelector('.card-info').appendChild(pCreator4);
+  divCreator.querySelector('.card-info').appendChild(pCreator5);
+  divCreator.querySelector('.card-info').appendChild(pCreator6);
+
+  return divCreator;
+};
+
 /*
   List of LS Instructors Github username's:
     tetondan
